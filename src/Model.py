eb4a8b9ea24be60d84b9baf25fa3e86f3842291d
sde <sagnikde2024@gmail.com>
2025-03-09 11:58:15 -0400

    Redesign. of Model.

--- src/Model.py
+++ src/Model.py
@@ -18,15 +18,42 @@ class Model(nn.Module):
         self.variance_upsample = nn.Upsample(size=(latent_size, latent_size))
 
 
-    def reparameterization(self, mean, var):
-        epsilon = torch.randn_like(var) # sampling epsilon
-        var_up = self.variance_upsample(var)
-        z = mean + var_up * epsilon  # reparameterization trick
+    @staticmethod
+    def reparameterization(mean, var):
+        epsilon = torch.randn_like(mean) # sampling epsilon
+
+        z = mean + var * epsilon  # reparameterization trick
         return z
 
     def forward(self,x):
         mu,log_var = self.encoder.forward(x)
-        var = torch.exp(log_var)
-        z_sample = self.reparameterization(mu, var)
+        var = torch.exp(0.5 * log_var)
+        var_up = self.variance_upsample(var)
+        z_sample = self.reparameterization(mu, var_up)
         decoded = self.decoder(z_sample)
-        return mu, log_var, decoded
+        return mu, var, decoded
+
+    def freeze_encoder(self):
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+    def unfreeze_encoder(self):
+        for param in self.encoder.parameters():
+            param.requires_grad = True
+
+    def freeze_decoder(self):
+        for param in self.decoder.parameters():
+            param.requires_grad = False
+
+    def unfreeze_decoder(self):
+        for param in self.decoder.parameters():
+            param.requires_grad = True
+
+    def generate(self,given_sample):
+        mu, log_var = self.encoder.forward(given_sample)
+        var = torch.exp(0.5 * log_var)
+        def get_new_sample():
+            z_sample = self.reparameterization(mu, var)
+            decoded = self.decoder(z_sample)
+            return z_sample, decoded
+        return get_new_sample()
