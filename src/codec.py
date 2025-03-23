c04d11b7ac9dee351792ffc8875394402e056009
sde <sagnikde2024@gmail.com>
2025-03-22 20:51:12 -0400

    Some codec stuff...

--- src/codec.py
+++ src/codec.py
@@ -1,4 +1,6 @@
 import logging
+from dataclasses import dataclass
+from math import log2
 from typing import List
 
 from torch import nn
@@ -7,22 +9,51 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-# A k x k kernel with c_in input channels and c_out output channels will have params = (k*k*c_in + 1)*c_out
-# This will create two kernels 1 x k and k x 1 with intermediate number of features c_intermediate so that it has  (ratio_to_k2) * params parameters
+@dataclass
+class NewSize:
+    starting_size: int
+    ratio: float
 
 
-def generate_separated_kernels(k_size: int, input_channel: int, output_channel: int, ratio_to_k2: float,
+@dataclass
+class SquareKernel:
+    kernel_size: int
+
+
+@dataclass
+class SeparatedKernel:
+    kernel_size: int
+    parameter_ratio: float
+    intermediate_ratio: float
+
+
+CNNKernel = SquareKernel | SeparatedKernel
+
+
+# The function will make a 1×k kernel followed by a k×1 kernel with c_in^(1-a)×c_out^a intermediate channels.
+# By default, c_intermediate is geometric mean of c_in and c_out i.e. a = 1/2.
+# The other parameter that can be used is 'r' which is the ratio of parameters of an original k×k kernel.
+# Since a k×k kernel has (k×k×c_in + 1)×c_out parameters, the number total number of parameters
+# of the new two kernels will be roughly r×(k×k×c_in + 1)×c_out parameters.
+# The 'a' will be calculated automatically. It will warn if the calculated 'a' falls outside [0,1].
+
+
+def generate_separated_kernels(k_size: int, input_channel: int, output_channel: int, a=(1 / 2), r=0.0,
                                add_padding=True):
-    r = ratio_to_k2
     c_in = input_channel
     c_out = output_channel
+    t = c_out / c_in
     k = k_size
-    c_intermediate = int(round((c_out * (-1 + r + c_in * (k ** 2) * r)) / (1 + (c_in + c_out) * k), 0))
-    if c_in != c_out:
-        intermediate_location = (c_intermediate - c_in) / (c_out - c_in)
-        if not (0 <= intermediate_location <= 1):
+
+    if 0 < r < 1 and t != 1:
+        a = log2(c_in * k ** 2 * r + r - 1) / log2(t) - log2(c_in * k * (t + 1) + 1) / log2(t) + 1
+
+    c_intermediate = int(round((c_in ** (1 - a) * c_out ** a), 0))
+
+    if not (0 <= a <= 1):
             logger.warn(
                 f'Inconsistency in intermediate features: c_in={c_in}, c_intermediate={c_intermediate}, c_out={c_out}. {c_intermediate} ∉ [{c_in},{c_out}]')
+
     if add_padding:
         padding = k // 2
         conv_layer_1 = nn.Conv2d(c_in, c_intermediate, (k, 1), padding=(padding, 0))
@@ -34,6 +65,19 @@ def generate_separated_kernels(k_size: int, input_channel: int, output_channel: 
         return nn.Sequential(conv_layer_1, conv_layer_2)
 
 
+def conv_kernel(k_size: int, input_channel: int, output_channel: int, separable, ratio):
+    if separable:
+        if ratio > 1:
+            return generate_separated_kernels(k_size, input_channel, output_channel, add_padding=False)
+        else:
+            return generate_separated_kernels(k_size, input_channel, output_channel, add_padding=True)
+    else:
+        if ratio > 1:
+            return nn.Conv2d(input_channel, output_channel, kernel_size=k_size)
+        else:
+            return nn.Conv2d(input_channel, output_channel, kernel_size=k_size, padding=k_size // 2)
+
+
 def channel_kernel_compute(inp_out_channels: List[int], layers):
     in_channel = inp_out_channels[0]
     out_channel = inp_out_channels[1]
@@ -43,7 +87,7 @@ def channel_kernel_compute(inp_out_channels: List[int], layers):
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_size, output_size, kernel_sizes=None, channels=None):
+    def __init__(self, input_size, output_size, kernel_sizes=None, channels=None, last_activation=nn.Tanh()):
         super().__init__()
         size = input_size
         layers = len(kernel_sizes)
@@ -56,16 +100,16 @@ class Decoder(nn.Module):
             ch_next = channels[layer + 1]
             kernel_size = kernel_sizes[layer]
             k_1 = kernel_size - 1
-            upsample_layer = nn.UpsamplingBilinear2d(size=(up_size + k_1, up_size + k_1))
+            upsample_layer = nn.UpsamplingBilinear2d(size=up_size + k_1)
             sequence.append(upsample_layer)
-            conv_layer = nn.Conv2d(ch_in, ch_next, kernel_size)
+            conv_layer = nn.Conv2d(ch_in, ch_next, kernel_size) if kernel_size <= 3 else generate_separated_kernels(
+                kernel_size, ch_in, ch_next, r=9 / 25, add_padding=False)
             sequence.append(conv_layer)
             if layer < layers - 1:
                 activation_layer = nn.Mish()
                 sequence.append(activation_layer)
-                # sequence.append(nn.BatchNorm2d(ch_next))
             else:
-                activation_layer = nn.Tanh()
+                activation_layer = last_activation
                 sequence.append(activation_layer)
         self.sequence = nn.Sequential(*sequence)
 
@@ -99,7 +143,7 @@ class Encoder(nn.Module):
             chin, chout = channels[layer], channels[layer + 1]
             kernel_size = kernel_sizes[layer]
             # padding = kernel_size // 2
-            conv_layer = generate_separated_kernels(kernel_size, chin, chout, 1.125 * 2 / kernel_size)
+            conv_layer = generate_separated_kernels(kernel_size, chin, chout)
 
             # conv_layer = nn.Conv2d(chin, chout, kernel_size, padding=padding)
             activation_layer = nn.Mish()
@@ -125,8 +169,4 @@ class Encoder(nn.Module):
         return enc
 
     def forward(self, input_x):
-        layered_result = self.sequence.forward(input_x)
-        (n, c, h, w) = layered_result.size()
-        mean = layered_result[:, :c // 2, :, :]
-        log_var = layered_result[:, c // 2:, :, :]
-        return mean, log_var
+        return self.sequence.forward(input_x)
