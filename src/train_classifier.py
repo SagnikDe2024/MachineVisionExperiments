d3d2b2304f22f07eb07b8172de13eb12f917f5a4
sde <sagnikde2024@gmail.com>
2025-03-24 19:35:32 -0400

    Classifier training.

---
+++ src/train_classifier.py
@@ -0,0 +1,77 @@
+import torch
+from torch import nn
+from torchinfo import summary
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import transforms
+
+from src.Model import Classifier
+from src.common_utils import AppLog
+
+
+def load_cifar_dataset():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5,), (0.5,))])
+    training_set = CIFAR10(root='../data/CIFAR/train', train=True, download=True, transform=transform)
+    validation_set = CIFAR10(root='../data/CIFAR/test', train=False, download=True, transform=transform)
+    AppLog.info(f'{len(training_set)} training samples and {len(validation_set)} validation samples')
+    train_loader = torch.utils.data.DataLoader(training_set, batch_size=96, shuffle=True)
+    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=96, shuffle=False)
+    return train_loader, validation_loader
+
+
+if __name__ == '__main__':
+
+    train_loader, validation_loader = load_cifar_dataset()
+
+    classifier = Classifier([384, 62, 10]).cuda()
+    AppLog.info(f'{summary(classifier, input_size=(128, 3, 32, 32))}')
+    learning_rate = 0.001
+    optimizer = torch.optim.Adam(classifier.parameters(), lr=learning_rate)
+    loss_fn = nn.CrossEntropyLoss()
+
+    EPOCHS = 5
+    best_vloss = float('inf')
+    AppLog.info(f'Starting {EPOCHS} epochs with learning rate {learning_rate}')
+
+    for epoch in range(EPOCHS):
+        classifier.train(True)
+        running_loss = 0.0
+        train_batch_index = 0
+        for img, label in train_loader:
+            img = img.cuda()
+            label = label.cuda()
+            train_batch_index += 1
+
+            raw_prob, prob = classifier.forward(img)
+            loss = loss_fn(raw_prob, label)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            AppLog.info(
+                f'Epoch [{epoch + 1}/{EPOCHS}]: Batch [{train_batch_index}]: Loss: {running_loss / train_batch_index}')
+
+        # Evaluate the classifier.
+        classifier.eval()
+        running_vloss = 0.0
+        valid_batch_index = 0
+        with torch.no_grad():
+            for img, label in validation_loader:
+                img, label = img.cuda(), label.cuda()
+                img = img.cuda()
+                raw_prob, prob = classifier.forward(img)
+                v_loss = loss_fn(raw_prob, label)
+                running_vloss += v_loss.item()
+                valid_batch_index += 1
+                AppLog.info(
+                    f'Epoch [{epoch + 1}/{EPOCHS}]: V_Batch [{valid_batch_index}]: V_Loss: {running_loss / (valid_batch_index)}')
+
+        avg_loss = running_loss / train_batch_index
+        avg_vloss = running_vloss / valid_batch_index
+        AppLog.info(f'Epoch {epoch + 1}: Training loss = {avg_loss}, Validation Loss = {avg_vloss}')
+
+        if avg_vloss < best_vloss:
+            best_vloss = avg_vloss
+            torch.save(classifier.state_dict(), f'../models/classifier_{epoch + 1}')
+
+    AppLog.info(f'Classifier best vloss: {best_vloss}, training done')
