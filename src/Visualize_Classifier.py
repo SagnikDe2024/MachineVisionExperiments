afde39d4412967ce8637a7d2989ace1baf3f3004
sde <sagnikde2024@gmail.com>
2025-03-28 17:09:26 -0400

    Pointless fix.

--- src/Visualize_Classifier.py
+++ src/Visualize_Classifier.py
@@ -8,51 +8,50 @@ from src.utils.common_utils import AppLog
 
 
 def show_images(img_b) -> None:
-    img = img_b.numpy().transpose((1, 2, 0))
-    un_normal = img * 0.5 + 0.5
+	img = img_b.numpy().transpose((1, 2, 0))
+	un_normal = img * 0.5 + 0.5
 
 
-def load_model(model_name):
-    classifier_params, model_state = torch.load(f'c:/mywork/python/ImageEncoderDecoder/models/{model_name}')
-    saved_classifier = Classifier(**classifier_params)
-    saved_classifier.load_state_dict(model_state)
-    return saved_classifier
+def load_model(model_name) -> Classifier:
+	classifier_params, model_state = torch.load(f'c:/mywork/python/ImageEncoderDecoder/models/{model_name}')
+	saved_classifier = Classifier(**classifier_params)
+	saved_classifier.load_state_dict(model_state)
+	return saved_classifier
 
 
 def get_state_and_show(file_name) -> None:
-    saved_classifier = load_model(file_name)
-    saved_classifier = saved_classifier.cuda()
-    saved_classifier.eval()
-    tr, valid = load_cifar_dataset(1000)
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    correct_pred = {classname: 0 for classname in classes}
-    total_pred = {classname: 0 for classname in classes}
-    AppLog.info(f'Showing perf of {file_name}')
+	saved_classifier = load_model(file_name)
+	saved_classifier = saved_classifier.cuda()
+	saved_classifier.eval()
+	tr, valid = load_cifar_dataset(1000)
+	classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+	correct_pred = {classname: 0 for classname in classes}
+	total_pred = {classname: 0 for classname in classes}
+	AppLog.info(f'Showing perf of {file_name}')
 
-    with torch.no_grad():
-        for img, labels in tr:
-            img = img.cuda()
-            result, normed = saved_classifier.forward(img)
-            pred_labels = torch.argmax(normed, dim=1).cpu()
+	with torch.no_grad():
+		for img, labels in tr:
+			img = img.cuda()
+			result, normed = saved_classifier.forward(img)
+			pred_labels = torch.argmax(normed, dim=1).cpu()
 
-            # conf_matrix = multiclass_confusion_matrix(pred_labels, labels, 10)
+			# conf_matrix = multiclass_confusion_matrix(pred_labels, labels, 10)
 
-            for label, prediction in zip(labels, pred_labels):
-                if label == prediction:
-                    correct_pred[classes[label]] += 1
-                total_pred[classes[label]] += 1
+			for label, prediction in zip(labels, pred_labels):
+				if label == prediction:
+					correct_pred[classes[label]] += 1
+				total_pred[classes[label]] += 1
 
-    for classname, correct_count in correct_pred.items():
-        accuracy = 100 * float(correct_count) / total_pred[classname]
-        AppLog.info(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+	for classname, correct_count in correct_pred.items():
+		accuracy = 100 * float(correct_count) / total_pred[classname]
+		AppLog.info(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
 
 
 def show_model_accuracy() -> None:
-    items = os.scandir('c:/mywork/python/ImageEncoderDecoder/models')
-    for item in items:
-        if item.is_file():
-            if item.name.endswith('.pth'):
-                get_state_and_show(item.name)
+	items = os.scandir('c:/mywork/python/ImageEncoderDecoder/models')
+	for item in items:
+		if item.is_file():
+			if item.name.endswith('.pth'):
+				get_state_and_show(item.name)
 
-
-show_model_accuracy()
+# show_model_accuracy()
