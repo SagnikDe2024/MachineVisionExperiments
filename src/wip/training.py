81c6c06923fd0f0023a35e545f8a166475210f36
sde <sagnikde2024@gmail.com>
2025-03-28 02:23:35 -0400

    Functional state for training.py

--- src/wip/training.py
+++ src/wip/training.py
@@ -1,7 +1,6 @@
 import os
 import tempfile
-from multiprocessing import Process
-from queue import Queue
+from multiprocessing import Queue
 
 import torch
 from ray import tune
@@ -13,8 +12,8 @@ from src.utils.common_utils import AppLog
 
 
 class TrainModel:
-    def __init__(self, save_pooling, model, loss_fn, optimizer, device, starting_epoch, ending_epoch):
-        self.save_pooling = save_pooling
+    def __init__(self, save_checkpoint_epoch, model, loss_fn, optimizer, device, starting_epoch, ending_epoch):
+        self.save_checkpoint_epoch = save_checkpoint_epoch
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
@@ -30,7 +29,7 @@ class TrainModel:
         epoch = self.current_epoch
         EPOCHS = self.ending_epoch
         for img, label in train_loader:
-            # img, label = img.to(self.device), label.to(self.device)
+            img, label = img.to(self.device), label.to(self.device)
             self.optimizer.zero_grad()
             train_batch_index += 1
 
@@ -52,7 +51,7 @@ class TrainModel:
         EPOCHS = self.ending_epoch
         with torch.no_grad():
             for img, label in validation_loader:
-                # img, label = img.to(device), label.to(device)
+                img, label = img.to(self.device), label.to(self.device)
                 raw_prob, prob = self.model.forward(img)
                 v_loss = self.loss_fn(raw_prob, label)
                 running_vloss += v_loss.item()
@@ -73,7 +72,7 @@ class TrainModel:
 
             AppLog.info(f'Epoch {self.current_epoch + 1}: Training loss = {avg_loss}, Validation Loss = {avg_vloss}')
 
-            self.save_pooling.send((avg_vloss, self.model, self.current_epoch,))
+            self.save_checkpoint_epoch(avg_vloss, self.model, self.current_epoch)
             if avg_vloss < self.best_vloss:
                 self.best_vloss = avg_vloss
             elif avg_vloss > loss_best_threshold * self.best_vloss:
@@ -93,31 +92,28 @@ class TrainModel:
         return self.best_vloss, self.model.model_params
 
 
+# serialization_queue = Queue(maxsize=40)
+
+
 class ExperimentModels:
-
-    @classmethod
-    def save_checkpoint(cls, avg_vloss: float, classifier: nn.Module, epoch: int):
-        with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
-            model_name_temp = f'classifier_tuned.pth'
-            torch.save((classifier.model_params, classifier.state_dict()),
-                os.path.join(temp_checkpoint_dir, model_name_temp))
-            checkpoint = Checkpoint.from_directory(temp_checkpoint_dir)
-            tune.report({'v_loss': avg_vloss, 'epoch': (epoch + 1)}, checkpoint=checkpoint)
-
-    @classmethod
-    def queued_result(cls, q: Queue):
-        avg_vloss, classifier, epoch = q.get()
-        cls.save_checkpoint(avg_vloss, classifier, epoch)
 
     def __init__(self, model_creator_func, loader_func):
         self.model_creator_func = model_creator_func
         self.loader_func = loader_func
-        self.serialization_queue = Queue(maxsize=40)
-        self.save_process = Process(target=self.queued_result, args=(self.serialization_queue,))
-        self.save_process.start()
 
-    def send_checkpoint(self, check_params):
-        self.serialization_queue.put(check_params)
+        # self.save_process = Process(target=self.queued_result, args=(serialization_queue,))  # self.save_process.start()
+
+    def save_checkpoint(self, avg_vloss: float, classifier: nn.Module, epoch: int):
+        with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
+            model_name_temp = f'classifier_tuned.pth'
+            torch.save((classifier.model_params, classifier.state_dict()),
+                       os.path.join(temp_checkpoint_dir, model_name_temp))
+            checkpoint = Checkpoint.from_directory(temp_checkpoint_dir)
+            tune.report({'v_loss': avg_vloss, 'epoch': (epoch + 1)}, checkpoint=checkpoint)
+
+    def queued_result(self, q: Queue):
+        avg_vloss, classifier, epoch = q.get()
+        self.save_checkpoint(avg_vloss, classifier, epoch)
 
     def execute_single_experiment(self, model_config, batch_size, lr):
         model = self.model_creator_func(model_config)
@@ -127,16 +123,14 @@ class ExperimentModels:
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
         loss_fn = nn.CrossEntropyLoss()
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        trainable_params = model_summary.trainable_params()
+        trainable_params = model_summary.trainable_params
         AppLog.info(f'There are {trainable_params} trainable parameters.')
 
-        train_model = TrainModel(self.send_checkpoint, model, loss_fn, optimizer, device, 0, 50)
+        train_model = TrainModel(self.save_checkpoint, model, loss_fn, optimizer, device, 0, 50)
         best_vloss, model_params = train_model.train_and_evaluate(train_loader, validation_loader)
         AppLog.info(
             f'Best vloss: {best_vloss}, with {trainable_params} params. Performance per param (Higher is better) = {1 / (trainable_params * best_vloss)}')
         AppLog.info(f'Classifier best vloss: {best_vloss}, training done. Model params: {model_params}.')
         return {'v_loss': best_vloss, 'trainable_params': trainable_params, 'model_params': model_params}
 
-    def shutdown_checkpoint(self):
-        self.save_process.close()
-        self.save_process.join()
+    # def shutdown_checkpoint(self):  #     self.save_process.close()  #     self.save_process.join()
