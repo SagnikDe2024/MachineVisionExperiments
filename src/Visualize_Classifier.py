d2550401a5c4f5895bf25ea4fd8bc41bb453a25b
sde <sagnikde2024@gmail.com>
2025-04-07 01:46:27 -0400

    pandas fix ... ugh !

--- src/Visualize_Classifier.py
+++ src/Visualize_Classifier.py
@@ -1,8 +1,12 @@
+import json
+from pathlib import Path
+
 import torch
+from pandas import read_csv
 from torchinfo import summary
 
 from src.classifier.classifier import Classifier
-from src.train_classifier import load_cifar_dataset
+from src.tune_classifier import load_cifar_dataset
 from src.utils.common_utils import AppLog
 
 
@@ -20,6 +24,42 @@ def load_model(model_name) -> Classifier:
 	saved_classifier = Classifier(**classifier_params)
 	saved_classifier.load_state_dict(model_state)
 	return saved_classifier
+
+
+def removed_empty_checkpoint_directory():
+	working_dir = Path.cwd()
+	classifier_checkpoints_dir = working_dir / 'checkpoints' / 'tune_classifier'
+	sub_directories = [sub for sub in classifier_checkpoints_dir.iterdir() if sub.is_dir()]
+	for sub_directory in sub_directories:
+		stored_checkpoint_dirs = [chk for chk in sub_directory.iterdir() if chk.is_dir()]
+
+		if len(stored_checkpoint_dirs) == 0:
+			print(f'No checkpoint directory found for {sub_directory}')
+			stored_checkpoint_files = [chk for chk in sub_directory.iterdir() if chk.is_file()]
+			for file in stored_checkpoint_files:
+				file.unlink()
+			sub_directory.rmdir()
+			continue
+
+
+def find_classify_checkpoint():
+	working_dir = Path.cwd()
+	classifier_checkpoints_dir = working_dir / 'checkpoints' / 'tune_classifier'
+	sub_directories = [sub for sub in classifier_checkpoints_dir.iterdir() if sub.is_dir()]
+	for sub_directory in sub_directories:
+		# stored_checkpoint_dirs = [chk for chk in sub_directory.iterdir() if chk.is_dir()]
+		tune_params = json.load(open(sub_directory / 'params.json'))
+		# tune_params = read_json(sub_directory / 'params.json')
+		progress = read_csv(sub_directory / 'progress.csv')
+		min_vloss = progress.v_loss.min()
+
+		checkpoint_dir_min_vloss_index = progress[
+			progress.v_loss == min_vloss].index[0]
+		checkpoint_dir_min_vloss = progress.at[checkpoint_dir_min_vloss_index, 'checkpoint_dir_name']
+
+		print(f'Tuning {sub_directory} has vloss {min_vloss}')
+		print(checkpoint_dir_min_vloss)
+		print(f'Parameters: {tune_params}')
 
 
 def get_state_and_show(file_name) -> None:
@@ -60,4 +100,6 @@ def show_model_accuracy() -> None:
 
 
 if __name__ == '__main__':
-	show_model_accuracy()
+	find_classify_checkpoint()
+# removed_empty_checkpoint_directory()
+# show_model_accuracy()
