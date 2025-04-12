73e5cf0596214f1eb2b4c17d8324fd63be9f8fe9
sde <sagnikde2024@gmail.com>
2025-04-12 16:50:02 -0400

    Still not done ... dammit.

--- src/image_encoder_decoder/prepare_data.py
+++ src/image_encoder_decoder/prepare_data.py
@@ -1,10 +1,179 @@
+import itertools
+import pickle
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
 import torch
+from torch.utils.data import DataLoader, Dataset
+from torchvision.io import decode_image
+from torchvision.transforms import InterpolationMode
+from torchvision.transforms.v2.functional import crop_image, normalize, rotate, to_dtype
+
+from src.utils.common_utils import AppLog
+
+
+class RawImageDataSet(Dataset):
+	def __init__(self, directory: Path):
+		super().__init__()
+		self.directory = directory
+		image_files = itertools.chain(directory.glob("*.jpg"), directory.glob("*.jpeg"), directory.glob("*.png"))
+		self.image_files = list(image_files)
+
+	def __len__(self):
+		return len(self.image_files)
+
+	def __getitem__(self, idx):
+		image_path = self.image_files[idx]
+		float_tensor_image = to_dtype(decode_image(image_path, mode='RGB'), dtype=torch.float32, scale=True)
+		# Generate an id for the image
+		image_id = hash(pickle.dumps(float_tensor_image))
+		return image_id, float_tensor_image
+
+
+class Dice(torch.nn.Module):
+	def __init__(self, size, seed=0):
+		super().__init__()
+		self.size = size
+		self.seed = seed
+		self.rand_cuda = torch.Generator(device='cuda')
+		if seed != 0:
+			self.rand_cuda.manual_seed(seed)
+
+	def forward(self, pic, random_slice=False):
+		pic = torch.squeeze(pic)
+		(c, h, w) = pic.shape
+
+		if random_slice:
+			top_lefts = self.random_cropping(h, w)
+		else:
+			h_slices = h // self.size
+			w_slices = w // self.size
+			rest_h = h - h_slices * self.size
+			rest_w = w - w_slices * self.size
+			last_h = torch.tensor([h - self.size], dtype=torch.int32) if rest_h > 0 else torch.empty(0,
+																									 dtype=torch.int32)
+			last_w = torch.tensor([w - self.size], dtype=torch.int32) if rest_w > 0 else torch.empty(0,
+																									 dtype=torch.int32)
+			h_offsets = torch.arange(0, h_slices, dtype=torch.int32) * self.size
+			w_offsets = torch.arange(0, w_slices, dtype=torch.int32) * self.size
+			all_h = torch.concat([h_offsets, last_h])
+			all_w = torch.concat([w_offsets, last_w])
+			top_lefts = torch.cartesian_prod(all_h, all_w)
+
+		# AppLog.info(f'Calculated top_lefts = {top_lefts}')
+		diff_h, diff_w, prepared_image = read_norm_and_getdiffs(pic)
+		diff_h_min = diff_h.amin(dim=[1, 2])
+
+		# std here basically means range. std is used as that's what pytorch normalize says
+
+		diff_h_std = diff_h.amax(dim=[1, 2]) - diff_h_min
+		diff_w_min = diff_w.amin(dim=[1, 2])
+		diff_w_std = diff_w.amax(dim=[1, 2]) - diff_w_min
+
+		diff_h_min_tolist = diff_h_min.tolist()
+		diff_h_std_tolist = diff_h_std.tolist()
+		diff_w_min_tolist = diff_w_min.tolist()
+		diff_w_std_tolist = diff_w_std.tolist()
+
+		diff_h_norm = normalize(diff_h, diff_h_min_tolist, diff_h_std_tolist)
+
+		diff_w_norm = normalize(diff_w, diff_w_min_tolist, diff_w_std_tolist)
+
+		data = {'diff_h_min_rgb': diff_h_min_tolist,
+				'diff_h_std_rgb': diff_h_std_tolist,
+				'diff_w_min_rgb': diff_w_min_tolist,
+				'diff_w_std_rgb': diff_w_std_tolist,
+				'height'        : h,
+				'width'         : w}
+
+		def get_slices(top_left):
+			t = top_left[0]
+			l = top_left[1]
+			cropped_image = crop_image(pic, t, l, self.size, self.size)
+			cropped_diff_h = crop_image(diff_h_norm, t, l, self.size, self.size)
+			cropped_diff_w = crop_image(diff_w_norm, t, l, self.size, self.size)
+			return t, l, cropped_image, cropped_diff_h, cropped_diff_w
+
+		slices = list(map(get_slices, top_lefts))
+		data['slices'] = slices
+
+		return data
+
+	def random_cropping(self, h: int, w: int):
+		times = int((h // self.size) * (w // self.size) * 4 / 3)
+		AppLog.info(f'Random cropping with times: {times}')
+		rand_slices = torch.rand((2, times), generator=self.rand_cuda, requires_grad=False)
+		top_and_lefts = torch.concat([rand_slices[0] * (h - self.size), rand_slices[1] * (w - self.size)]).to(
+				dtype=torch.int32)
+		top_lefts = torch.permute(top_and_lefts, (1, 0))
+		return top_lefts
+
+
+def read_norm_and_getdiffs(float_tensor_image):
+
+	prepared_image = normalize(float_tensor_image, 0.5, 0.5)
+	image_mean = prepared_image.mean(dim=[-2, -1]).tolist()
+	image_std = prepared_image.std(dim=[-2, -1]).tolist()
+	AppLog.info(f' mean = {image_mean} and std = {image_std}')
+
+	img_45 = rotate(prepared_image, 45, interpolation=InterpolationMode.BILINEAR, expand=True)
+	(_, h45, w45) = img_45.shape
+	(_, h, w) = prepared_image.shape
+	top, left = (h45 - h) // 2, (w45 - w) // 2
+	diff_45_w = torch.diff(img_45, dim=-1)
+	diff_45_h = torch.diff(img_45, dim=-2)
+
+	diff_rotback_w = rotate(diff_45_w, -45, interpolation=InterpolationMode.BILINEAR, expand=False)
+	diff_rotback_h = rotate(diff_45_h, -45, interpolation=InterpolationMode.BILINEAR, expand=False)
+	diff_rotback_w_cropped = crop_image(diff_rotback_w, top, left, h, w)
+
+	diff_rotback_h_cropped = crop_image(diff_rotback_h, top, left, h, w)
+
+	diff_w = diff_rotback_w_cropped - diff_rotback_h_cropped
+	diff_h = diff_rotback_w_cropped + diff_rotback_h_cropped
+
+	return diff_h, diff_w, prepared_image
+
+
 class PrepareData:
-	def __init__(self):
-		raw_data_location = 'C:\mywork\ml\MachineVisionExperiments\data\raw'
-		prepared_data_location = 'C:\mywork\ml\MachineVisionExperiments\data\prepared'
+	def __init__(self, in_location: Path, out_location: Path):
+		self.in_location = in_location
+		self.out_location = out_location
+		self.dataset = RawImageDataSet(in_location)
+		# self.dicing = Dice(128)
+		self.dicing = torch.compile(Dice(128), mode="max-autotune")
 
-	def prepare_image(self, image_path):
-		image_name = torch.load(image_path,mode= )
+	def prepare_images(self):
+		AppLog.info(f'There are {len(self.dataset)} images')
+		self.dicing.cuda()
 
+		data_loader = DataLoader(self.dataset)
 
+		for data in data_loader:
+			img_id, tensor_image = data
+			(n, c, h, w) = tensor_image.shape
+			tensor_image = tensor_image.cuda()
+			data = self.dicing.forward(tensor_image)
+			# AppLog.info(f'Diced images {diced_images}')
+			diced_images = data['slices']
+			diced_images = map(lambda y: (y[0].cpu(), y[1].cpu(), y[2].cpu(), y[3].cpu(), y[4].cpu()), diced_images)
+			for diced_image in diced_images:
+				top, left, img_slice, diff_h_slice, diff_w_slice = diced_image
+				np_img = torch.permute(img_slice, (1, 2, 0)).numpy()
+				np_hdiff = torch.permute(diff_h_slice, (1, 2, 0)).numpy()
+				np_wdiff = torch.permute(diff_w_slice, (1, 2, 0)).numpy()
+
+			numpy_data = np.array(list(diced_images))
+			AppLog.info(f'The shape is {numpy_data.shape}')
+
+			dataframe = pd.DataFrame(numpy_data)
+			column_names = ['top', 'left', 'pic', 'diff_h', 'diff_w']
+			dataframe.columns = column_names
+			dataframe['image_id'] = img_id
+			dataframe['height'] = h
+			dataframe['width'] = w
+			AppLog.info(f'dataframe  = {dataframe}')
+			AppLog.info(f'dataframe shape = {dataframe.shape}')
+
+			dataframe.to_csv(self.out_location / 'img_id', index=True)
