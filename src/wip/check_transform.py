b556e1c80b0be19b15afc83eeb29937a2c52fa0e
sde <sagnikde2024@gmail.com>
2025-04-12 23:25:44 -0400

    Some more fixes ... test_prepdata() is broken though.

--- src/wip/check_transform.py
+++ src/wip/check_transform.py
@@ -9,7 +9,7 @@ from torchvision import transforms
 from torchvision.io import ImageReadMode, decode_image, write_jpeg
 from torchvision.transforms.v2.functional import InterpolationMode, crop_image, normalize, resize_image, rotate
 
-from src.image_encoder_decoder.prepare_data import PrepareData, read_norm_and_getdiffs
+from src.image_encoder_decoder.prepare_data import PrepareData, normalize_and_getdiffs
 from src.utils.common_utils import AppLog
 
 below = 0.01
@@ -21,7 +21,9 @@ def conv_test(image_file: str) -> None:
 	# transform = v2.Compose([v2.Resize((512, 512))])
 	# resized = transform(img)
 	perform_fft_and_save(img)
-	# perform_fft_and_save(resized)
+
+
+# perform_fft_and_save(resized)
 
 
 def filter_before_writing(img):
@@ -123,36 +125,30 @@ def minimize_and_rotate(t1, times):
 		rot_image.append(t1)
 	return rot_image
 
-def find_multiscale_diff(rot_images : List[torch.Tensor], times, crop, scale=1.5, strength=0.5):
 
-	top,left,imgh,imgw = crop
+def find_multiscale_diff(rot_images: List[torch.Tensor], times, crop, scale=1.5, strength=0.5):
+
+	top, left, imgh, imgw = crop
 	print(f'Cropping {crop}')
-	zero_img = torch.zeros([1,imgh,imgw]).cuda()
+	zero_img = torch.zeros([1, imgh, imgw]).cuda()
 	for t in range(times):
 
-
-		for i,rot_image in enumerate(rot_images):
-			(c,h,w) = rot_image.shape
+		for i, rot_image in enumerate(rot_images):
+			(c, h, w) = rot_image.shape
 			# scale_used = (1/scale)**t
-			(hr,hw) = round((1/scale)**t*h), round((1/scale)**t*w)
-			print(f'Scaled values, {hr,hw} from {h,w}')
+			(hr, hw) = round((1 / scale) ** t * h), round((1 / scale) ** t * w)
+			print(f'Scaled values, {hr, hw} from {h, w}')
 			rot_image_resized = resize_image(rot_image, [hr, hw])
 			img_diff_rot = torch.diff(rot_image_resized)
-			orig_size_diff_rotated = resize_image(img_diff_rot,[h,w])
-			back_rot = -(45 + i*90)
+			orig_size_diff_rotated = resize_image(img_diff_rot, [h, w])
+			back_rot = -(45 + i * 90)
 			print(f'Back rotated image with angle {back_rot}')
-			rot_back = rotate(orig_size_diff_rotated,back_rot,interpolation=InterpolationMode.BILINEAR, expand=False)
-			orig_size = crop_image(rot_back, top,left,imgh,imgw)
+			rot_back = rotate(orig_size_diff_rotated, back_rot, interpolation=InterpolationMode.BILINEAR, expand=False)
+			orig_size = crop_image(rot_back, top, left, imgh, imgw)
 			diff_strength = strength ** t
 			zero_img = zero_img + diff_strength * torch.abs(orig_size)
 
 	return zero_img
-
-
-
-
-
-
 
 
 def use_frac_pool(t1, kernel, fraction, times: int):
@@ -191,7 +187,8 @@ def just_dosomething():
 	print(f'Max = {max_value}, Max2 = {max_2}, Max4 = {max_4}')
 	ones = torch.ones_like(img_diff)
 	# result = torch.where(img_diff < med_v2, torch.concat([zero_img, zero_img, img_diff / med_v2], dim=0),
-	# 			torch.where(img_diff < med_value, torch.concat([(img_diff - med_v2)/(med_value-med_v2),zero_img, ones], dim=0 ),
+	# 			torch.where(img_diff < med_value, torch.concat([(img_diff - med_v2)/(med_value-med_v2),zero_img,
+	# 			ones], dim=0 ),
 	# 						torch.concat([ones,(img_diff - med_value)/(max_value - med_value), zero_img], dim=0)))
 	result = result * 255
 	write_jpeg(result.to(dtype=torch.uint8).cpu(), "out/face_diff.jpg")
@@ -220,7 +217,7 @@ def false_colour(img_diff):
 def test_prepdata():
 	global h, w
 	prepareData = PrepareData()
-	diff_h, diff_w, h, prepared_image, w = read_norm_and_getdiffs('data/reddit_face.jpg')
+	diff_h, diff_w, h, prepared_image, w = normalize_and_getdiffs('data/reddit_face.jpg')
 	tot_diff = torch.pow(torch.pow(diff_h, 2) + torch.pow(diff_w, 2), 0.5)
 
 	diff_h_min = diff_h.amin(dim=[1, 2])
@@ -263,50 +260,50 @@ if __name__ == '__main__':
 # just_dosomething()
 
 # frac_pool = use_frac_pool(img, (2, 2), (2 / 3, 2 / 3), 10)
-	# # rotated_images = minimize_and_rotate(img, 6)
-	# trans_imgs = frac_pool
-	# for i, rotated_image in enumerate(trans_imgs):
-	# 	write_png(rotated_image.to(dtype=torch.uint8), f'../out/frac_pool_{i}_image.png')
+# # rotated_images = minimize_and_rotate(img, 6)
+# trans_imgs = frac_pool
+# for i, rotated_image in enumerate(trans_imgs):
+# 	write_png(rotated_image.to(dtype=torch.uint8), f'../out/frac_pool_{i}_image.png')
 
-	# img = img.to(dtype=torch.float32)
-	# img = img/255
-	# crop_size = 256
-	# padding = crop_size//4
-	# img = crop(img,400,1244 ,  crop_size,crop_size)
-	# increased_img = pad_image(img,padding=padding)
-	#
-	# increased_img_45 = rotate(increased_img,45,InterpolationMode.BILINEAR)
-	# increased_img_135 = rotate(increased_img,-45,InterpolationMode.BILINEAR)
-	# rot_x_diff = torch.diff(increased_img_45,dim=2)
-	# rot_y_diff = torch.diff(increased_img_135,dim=2)
-	#
-	# x_diff = rotate(rot_x_diff,-45,InterpolationMode.BILINEAR)
-	# y_diff = rotate(rot_y_diff,45,InterpolationMode.BILINEAR)
-	# x_diff_abs = torch.abs(x_diff)
-	# y_diff_abs = torch.abs(y_diff)
-	# max_diff = (x_diff_abs + y_diff_abs - 2*(x_diff_abs*y_diff_abs))/(2-x_diff_abs-y_diff_abs)
-	# cropped_diff = crop(max_diff,padding-1,padding-1, crop_size+2,crop_size+2)*255
+# img = img.to(dtype=torch.float32)
+# img = img/255
+# crop_size = 256
+# padding = crop_size//4
+# img = crop(img,400,1244 ,  crop_size,crop_size)
+# increased_img = pad_image(img,padding=padding)
+#
+# increased_img_45 = rotate(increased_img,45,InterpolationMode.BILINEAR)
+# increased_img_135 = rotate(increased_img,-45,InterpolationMode.BILINEAR)
+# rot_x_diff = torch.diff(increased_img_45,dim=2)
+# rot_y_diff = torch.diff(increased_img_135,dim=2)
+#
+# x_diff = rotate(rot_x_diff,-45,InterpolationMode.BILINEAR)
+# y_diff = rotate(rot_y_diff,45,InterpolationMode.BILINEAR)
+# x_diff_abs = torch.abs(x_diff)
+# y_diff_abs = torch.abs(y_diff)
+# max_diff = (x_diff_abs + y_diff_abs - 2*(x_diff_abs*y_diff_abs))/(2-x_diff_abs-y_diff_abs)
+# cropped_diff = crop(max_diff,padding-1,padding-1, crop_size+2,crop_size+2)*255
 
-	# img = rotate(img,45,InterpolationMode.BILINEAR)
-	# # img = resize(img,[256,256])
-	# complex_image = convert_rgb_to_complex(img)
-	# undiff_complex = convert_complex_to_rgb(complex_image).squeeze(0)
-	# write_png(undiff_complex.to(dtype=torch.uint8), "../out/complex_crop.png")
-	# diff = find_complex_diff(complex_image)
-	# diff_abs_max = diff.abs().max()
-	# diff_scaled = diff*255 / diff_abs_max
-	# rgb_image = convert_complex_to_rgb(diff_scaled).squeeze(0)
-	# # rgb_rotated_back = rotate(rgb_image,-45,InterpolationMode.BILINEAR)
+# img = rotate(img,45,InterpolationMode.BILINEAR)
+# # img = resize(img,[256,256])
+# complex_image = convert_rgb_to_complex(img)
+# undiff_complex = convert_complex_to_rgb(complex_image).squeeze(0)
+# write_png(undiff_complex.to(dtype=torch.uint8), "../out/complex_crop.png")
+# diff = find_complex_diff(complex_image)
+# diff_abs_max = diff.abs().max()
+# diff_scaled = diff*255 / diff_abs_max
+# rgb_image = convert_complex_to_rgb(diff_scaled).squeeze(0)
+# # rgb_rotated_back = rotate(rgb_image,-45,InterpolationMode.BILINEAR)
 
-	# write_png(cropped_diff.to(dtype=torch.uint8), "../out/complex_pipe_max_crop.png")
+# write_png(cropped_diff.to(dtype=torch.uint8), "../out/complex_pipe_max_crop.png")
 
-	# pdf = calculate_pdf(resized)
-	# log_pdf = np.log2(pdf,out = np.zeros_like(pdf, dtype=np.float32), where = (pdf != 0))
-	# multiplied = np.multiply(-log_pdf, pdf)
-	# entropy = np.sum(multiplied)
-	# print(f'Total entropy = {entropy}')
-	# plt.contourf(log_pdf[1])
-	# plt.axis('square')
-	# plt.show()
+# pdf = calculate_pdf(resized)
+# log_pdf = np.log2(pdf,out = np.zeros_like(pdf, dtype=np.float32), where = (pdf != 0))
+# multiplied = np.multiply(-log_pdf, pdf)
+# entropy = np.sum(multiplied)
+# print(f'Total entropy = {entropy}')
+# plt.contourf(log_pdf[1])
+# plt.axis('square')
+# plt.show()
 
-	# conv_test("../data/normal_pic.jpg")
+# conv_test("../data/normal_pic.jpg")
