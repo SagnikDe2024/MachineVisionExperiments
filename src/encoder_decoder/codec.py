a3cc9bf8862b5f65f65efae7976f068560fd3856
SagnikDe2024 <sagnikde2024@gmail.com>
2025-04-27 16:40:56 -0400

    Image compressor merged to develop.
    * Just commiting. Still WIP.
    
    * WIP.
    
    * Still not done ... dammit.
    
    * IDK
    
    * A simple show using matplotlib.
    
    * WIP.
    
    * WIP some code commented out.
    
    * Still WIP, deciding between doing the slicing when training or doing it separately beforehand.
    
    * Some more fixes ...
    test_prepdata() is broken though.
    
    * Updated requirements.
    
    * Made changes as necessary.
    
    * Wip stuff.
    
    * The decoder seems to be working ...
    
    * Added similar for Encoder block.
    
    * Some more changes.

--- src/encoder_decoder/codec.py
+++ src/encoder_decoder/codec.py
@@ -1,6 +1,10 @@
 import torch
 from numpy import log2
-from torch import nn
+from torch import Tensor, nn
+from torch.nn import ModuleDict
+from torch.nn.functional import interpolate
+from torchinfo import summary
+
 
 from src.common.common_utils import AppLog
 
@@ -26,6 +30,55 @@ def generate_separated_kernels(input_channel: int, output_channel: int, k_size: 
 	conv_layer_2 = nn.Conv2d(c_intermediate, c_out, (k, 1), padding=(padding, 0), bias=bias, stride=(stride, 1))
 
 	return conv_layer_1, conv_layer_2
+
+class EncoderLayer(nn.Module):
+	def __init__(self, input_channels, output_channels, kernel_ratios):
+		super().__init__()
+		total_ratios = sum(kernel_ratios)
+		mod_dic = ModuleDict()
+		out_channels = [int(round(output_channels * out_r / total_ratios, 0)) for out_r in kernel_ratios]
+		rest = sum(out_channels[1:]) if len(out_channels) > 1 else 0
+		out_channels[0] = output_channels - rest
+
+		for i, out_ch in enumerate(out_channels):
+			kernel_size = i * 2 + 3
+			output_channel = out_ch
+			if kernel_size == 3:
+				conv_layer = nn.Conv2d(in_channels=input_channels, out_channels=output_channel,
+									   kernel_size=kernel_size,
+									   padding=1, bias=False)
+				mod_dic[f'{kernel_size}'] = conv_layer
+
+				continue
+			conv_1, conv_2 = generate_separated_kernels(input_channels, output_channel, kernel_size, r=3 / kernel_size,
+														add_padding=True)
+			seq = nn.Sequential(conv_1, conv_2)
+			mod_dic[f'{kernel_size}'] = seq
+
+		self.conv_layers = mod_dic
+		self.norm = nn.BatchNorm2d(output_channels)
+		self.activation = nn.Mish()
+		self.pooling = nn.FractionalMaxPool2d(2,output_ratio=0.5**(1/7))
+
+
+
+	def forward(self, x):
+
+
+
+		convs = []
+		for conv_layer in self.conv_layers.values():
+			conv : Tensor = conv_layer.forward(x)
+			convs.append(conv)
+
+		concat_res = torch.cat(convs, dim=1)
+
+		normed_res = self.norm(concat_res)
+
+
+		return normed_res
+
+
 
 
 class InputLayer(nn.Module):
@@ -123,32 +176,123 @@ class Encoder(nn.Module):
 		return torch.concat([z_w, z_h], 1)
 
 
-class Decoder(nn.Module):
-	def __init__(self, channels=None, last_activation=nn.Tanh()) -> None:
-		super().__init__()
-		layers = 4
-		upscale_ratio = 2
-		AppLog.info(
-				f'Layers = {layers}, channels = {channels}')
+# def decoder_upscale_module(input_tensor, kernel_size):
+# 	upsample_add = kernel_size - 1
+#
+# 	interpolate(input_tensor,siz)
 
-		sequence = nn.Sequential()
-		for layer in range(layers):
+class DecoderLayer(nn.Module):
+	def __init__(self, input_channels, output_channels, kernel_ratios):
+		super().__init__()
+		total_ratios = sum(kernel_ratios)
+		mod_dic = ModuleDict()
+		out_channels = [int(round(output_channels * out_r / total_ratios, 0)) for out_r in kernel_ratios]
+		rest = sum(out_channels[1:]) if len(out_channels) > 1 else 0
+		out_channels[0] = output_channels - rest
+
+		for i, out_ch in enumerate(out_channels):
+			kernel_size = i * 2 + 3
+			output_channel = out_ch
+			if kernel_size == 3:
+				conv_layer = nn.Conv2d(in_channels=input_channels, out_channels=output_channel,
+									   kernel_size=kernel_size,
+									   padding=1, bias=False)
+				mod_dic[f'{kernel_size}'] = conv_layer
+
+				continue
+			conv_1, conv_2 = generate_separated_kernels(input_channels, output_channel, kernel_size, r=3 / kernel_size,
+														add_padding=True)
+			seq = nn.Sequential(conv_1, conv_2)
+			mod_dic[f'{kernel_size}'] = seq
+
+		self.conv_layers = mod_dic
+		self.norm = nn.BatchNorm2d(output_channels)
+
+		self.h = 128
+		self.w = 128
+
+	def set_h_w(self, h, w):
+		self.h = h
+		self.w = w
+
+	def forward(self, x):
+
+		upsampled = interpolate(x, size=(self.h, self.w), mode='bicubic')
+
+		convs = []
+		for conv_layer in self.conv_layers.values():
+			conv : Tensor = conv_layer.forward(upsampled)
+			convs.append(conv)
+
+		concat_res = torch.cat(convs, dim=1)
+
+		normed_res = self.norm(concat_res)
+
+		return normed_res
+
+
+class Decoder(nn.Module):
+	def __init__(self, in_channels, out_channels) -> None:
+		super().__init__()
+
+		layers = 6
+		self.layers = layers
+
+		channel_r = (out_channels / in_channels) ** (1 / (layers - 1))
+		channels = [int(round(in_channels * channel_r ** x, 0)) for x in range(layers)]
+		channels = [*channels, 3]
+
+		# AppLog.info(
+		# 		f'Layers = {layers}, channels = {channels}')
+
+		kernel_channel_ratios = [1, 3 / 5, 3 / 7]
+
+		decoder_layers = ModuleDict()
+		activation_layers = ModuleDict()
+		for layer in range(layers - 1):
 			ch_in = channels[layer]
 			ch_out = channels[layer + 1]
 
-			upsample_layer = nn.Upsample(scale_factor=upscale_ratio, mode='bicubic')
-			sequence.append(upsample_layer)
-			conv_layer_1, conv_layer_2 = generate_separated_kernels(ch_in, ch_out, 5, r=10 / 25, add_padding=True)
-			sequence.append(conv_layer_1)
-			sequence.append(conv_layer_2)
-			sequence.append(nn.BatchNorm2d(ch_out))
-			if layer < layers - 1:
-				activation_layer = nn.Mish()
-				sequence.append(activation_layer)
-			else:
-				activation_layer = last_activation
-				sequence.append(activation_layer)
-		self.sequence = nn.Sequential(*sequence)
+			dec_layer = DecoderLayer(ch_in, ch_out, kernel_channel_ratios)
+			decoder_layers[f'{layer}'] = dec_layer
+			activation_layers[f'{layer}'] = nn.Mish()
+		last_layer = DecoderLayer(channels[-2], channels[-1], [1])
+		decoder_layers['last'] = last_layer
+		activation_layers['last'] = nn.Tanh()
+		self.decoder_layers = decoder_layers
+		self.activation_layers = activation_layers
 
-	def forward(self, latent_z):
-		return self.sequence.forward(latent_z)
+		self.h = 128
+		self.w = 128
+
+	def set(self, h, w):
+		self.h = h
+		self.w = w
+
+	def forward(self, latent_z: torch.Tensor):
+		_, _, z_h, z_w = latent_z.shape
+		h_r = self.h / z_h
+		w_r = self.w / z_w
+		l = self.layers
+		h_s = [int(round(z_h * h_r ** ((r + 1) / l), 0)) for r in range(l)]
+		w_s = [int(round(z_w * w_r ** ((r + 1) / l), 0)) for r in range(l)]
+
+		z_m = latent_z
+		for dec_l, act_l, h, w in zip(self.decoder_layers.values(), self.activation_layers.values(), h_s, w_s):
+			dec_l.set_h_w(h, w)
+			z_m = dec_l.forward(z_m)
+			z_m = act_l.forward(z_m)
+
+		return z_m
+
+
+if __name__ == '__main__':
+	dec = Decoder(in_channels=288, out_channels=48)
+	# decl = DecoderLayer(64, 32, [1, 3 / 5, 3 / 7])
+	# for layer_name, params in decl.named_parameters():
+	# 	print(layer_name, params.shape)
+	dec.set(256, 256)
+	for layer_name, params in dec.named_parameters():
+		print(layer_name, params.shape)
+	summary(dec, input_size=(1, 288, 16, 16))
+# AppLog.shut_down()
