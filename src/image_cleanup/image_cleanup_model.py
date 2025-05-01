04803ac879a1ca95b92877bccf1f4240753d2954
sde <sagnikde2024@gmail.com>
2025-04-30 22:56:08 -0400

    Image cleaned up.

--- src/image_cleanup/image_cleanup_model.py
+++ src/image_cleanup/image_cleanup_model.py
@@ -3,10 +3,9 @@ from torch import nn
 from src.encoder_decoder.codec import DecoderLayer, EncoderLayer
 
 
-class ImageCleanupModel(nn.Module):
-	def __init__(self, encoder_channels, decoder_channels, total_downsample):
+class ImageCleanUpEncoder(nn.Module):
+	def __init__(self, encoder_channels, total_downsample) -> None:
 		super().__init__()
-
 		encoder_layers = len(encoder_channels) - 1
 		downsample_ratio = total_downsample ** (-1 / encoder_layers)
 
@@ -21,23 +20,45 @@ class ImageCleanupModel(nn.Module):
 											 downsample=downsample_ratio)
 			self.encoder.append(encoder_layer)
 
-		self.middle_depthwise_conv = nn.Conv2d(encoder_channels[-1], decoder_channels[0], kernel_size=1, padding=(1,
-																												  1))
+	def forward(self, x):
+		features = self.encoder(x)
+		return features
 
+
+class ImageCleanUpDecoder(nn.Module):
+	def __init__(self, decoder_channels, upsample) -> None:
+		super().__init__()
+		decoder_layers = len(decoder_channels) - 1
 		self.decoder = nn.ModuleDict()
 
-		decoder_layers = len(decoder_channels) - 1
-		decoder_activation = nn.Mish()
-		decoder_activation_last = nn.Softplus()
+		self.decoder_activation = nn.Mish()
+		self.decoder_activation_last = nn.Softplus()
+		upsample_ratio = upsample ** (-1 / decoder_layers)
 
 		for channel_index in range(decoder_layers):
 			input_channel = decoder_channels[channel_index]
 			output_channel = decoder_channels[channel_index + 1]
 			if channel_index == decoder_layers - 1:
-				decoder_layer = DecoderLayer(input_channel, output_channel, [(3, 1)])
+				decoder_layer = DecoderLayer(input_channel, output_channel, [(3, 1)], upsample_ratio)
 			else:
-				decoder_layer = DecoderLayer(input_channel, output_channel, [(3, 1), (5, 1)])
-
+				decoder_layer = DecoderLayer(input_channel, output_channel, [(3, 1), (5, 1)], upsample_ratio)
 			self.decoder[f'd_{channel_index}'] = decoder_layer
+		self.decoder_layers = decoder_layers
+
+	def forward(self, x):
+
+		for i, layer in enumerate(self.decoder.values()):
+			x = layer(x)
+			if i != self.decoder_layers - 1:
+				x = self.decoder_activation(x)
+			else:
+				x = self.decoder_activation_last(x)
+		return x
 
 
+class ImageCleanupModel(nn.Module):
+	def __init__(self, encoder_channels, decoder_channels, total_downsample):
+		super().__init__()
+
+		self.middle_depthwise_conv = nn.Conv2d(encoder_channels[-1], decoder_channels[0], kernel_size=1, padding=(1,
+																												  1))
