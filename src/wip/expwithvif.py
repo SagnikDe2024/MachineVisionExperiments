f7f98f26deb38f25c12d5f9fcbdf359f2d72281b
sde <sagnikde2024@gmail.com>
2025-06-22 00:35:39 -0400

    Integrated `MultiscalePerceptualLoss` into `expwithvif.py` and refactored functions for calculating VIF score and perceptual loss.

--- src/wip/expwithvif.py
+++ src/wip/expwithvif.py
@@ -1,16 +1,24 @@
 from src.common.common_utils import acquire_image
+from src.encoder_decoder.image_reconstruction_loss import MultiscalePerceptualLoss
 from src.image_cleanup.image_defects import add_jpeg_artifacts
 from torchmetrics.image import VisualInformationFidelity
+
+
+def calc_vif_score():
+	vif = VisualInformationFidelity()
+	vif_score = vif(img_acq, img_acq)
+	print(f'vif_score = {vif_score}')
+
+
+def calc_perceptual_loss():
+	perceptual_loss = MultiscalePerceptualLoss()
+	loss_value = perceptual_loss(img_acq, arty)
+	print(f'Perceptual loss between original and distorted image: {loss_value.item()}')
+
 
 if __name__ == "__main__":
 	img_acq = acquire_image('data/normal_pic.jpg')
 	img_acq = img_acq.unsqueeze(0)
-	arty = add_jpeg_artifacts(img_acq,100)
-	vif = VisualInformationFidelity()
-	vif_score = vif(img_acq, img_acq)
-
-	print(f'vif_score = {vif_score}')
-	print(f'diff = {VisualInformationFidelity.is_differentiable}')
-
-
-
+	arty = add_jpeg_artifacts(img_acq,90)
+	# calc_vif_score()
+	calc_perceptual_loss()
