abfebde89a8acb802d2ecf7e5a9d3801dc1b7577
sde <sagnikde2024@gmail.com>
2025-07-24 00:13:46 -0400

    Upsampling fix.

--- src/encoder_decoder/codec.py
+++ src/encoder_decoder/codec.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Optional, Tuple
 
 import torch
 from torch import Tensor, nn
@@ -47,38 +47,10 @@ class EncoderLayer(nn.Module):
 
 class DecoderLayer(nn.Module):
 
-	def __init__(self, input_channels, output_channels, kernels_and_ratios, upscale=None):
+	def __init__(self, input_ch, mid_ch, output_ch, stack: list[Tuple[int, int]] | Tuple[int, list[int]],upsample : Optional[int]=2):
 		super().__init__()
-		self.upscale = None
-		kernels, kernel_ratios = zip(*kernels_and_ratios)
-
-		total_ratios = sum(kernel_ratios)
-		mod_dic = ModuleDict()
-		out_channels = [int(round(output_channels * out_r / total_ratios, 0)) for out_r in kernel_ratios]
-		rest = sum(out_channels[1:]) if len(out_channels) > 1 else 0
-		out_channels[0] = output_channels - rest
-
-		for i, out_ch in enumerate(out_channels):
-			kernel_size = kernels[i]
-
-			output_channel = out_ch
-			if kernel_size == 3:
-				conv_layer = nn.Conv2d(in_channels=input_channels, out_channels=output_channel,
-				                       kernel_size=kernel_size,
-				                       padding=1, bias=False)
-				mod_dic[f'{kernel_size}'] = conv_layer
-				continue
-			conv_1, conv_2 = generate_separated_kernels(input_channels, output_channel, kernel_size, ParamA(1),
-			                                            add_padding=True)
-
-			seq = nn.Sequential(conv_1, conv_2)
-			if upscale is not None:
-				self.upscale = nn.Upsample(scale_factor=upscale, mode='bicubic')
-			mod_dic[f'{kernel_size}'] = seq
-
-		self.conv_layers = mod_dic
-		self.norm = nn.InstanceNorm2d(output_channels)
-
+		self.cnn_stack = CodecMultiKernelStack(input_ch, mid_ch, output_ch, stack)
+		self.upscale = nn.Upsample(scale_factor=upsample, mode='bicubic') if upsample > 1 else None
 		self.h = 128
 		self.w = 128
 
@@ -87,22 +59,12 @@ class DecoderLayer(nn.Module):
 		self.w = w
 
 	def forward(self, x: Tensor):
-
 		if self.upscale is not None:
 			upsampled = self.upscale(x)
 		else:
 			upsampled: Tensor = interpolate(x, size=(self.h, self.w), mode='bicubic')
-
-		convs = []
-		for conv_layer in self.conv_layers.values():
-			conv: Tensor = conv_layer.forward(upsampled)
-			convs.append(conv)
-
-		concat_res = torch.cat(convs, dim=1)
-
-		normed_res = self.norm(concat_res)
-
-		return normed_res
+		cnn_res = self.cnn_stack(upsampled)
+		return cnn_res
 
 
 def create_sep_kernels(input_channels, output_channels, kernel_size):
