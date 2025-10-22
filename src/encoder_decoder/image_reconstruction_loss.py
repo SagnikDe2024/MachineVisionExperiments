5c2f6f53dc1ac9c2b05d9cec46ec9bac6da35768
sde <sagnikde2024@gmail.com>
2025-10-22 18:18:53 -0400

    Refactored `forward` method in `image_codec.py` to simplify logic and improve handling of `h` and `w` parameters; updated `encode_decode_from_model` to reflect these changes.

--- src/encoder_decoder/image_reconstruction_loss.py
+++ src/encoder_decoder/image_reconstruction_loss.py
@@ -102,7 +102,7 @@ def get_saturation(image):
 class SaturationLoss(nn.Module):
 	def __init__(self):
 		super().__init__()
-		self.loss2 = nn.MSELoss()
+		self.loss = nn.SmoothL1Loss(beta=0.5)
 
 	def forward(self, inferred_image, target_image):
 		min_i = torch.min(inferred_image, dim=1, keepdim=True)
